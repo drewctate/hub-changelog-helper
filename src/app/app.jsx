4dd21c23bb0963d3bd45bb4fb72a08bf716b768b
Andrew Tate <drewctate@gmail.com>
2020-03-20 10:11:22 -0600

    Copied confirmation text

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -7,7 +7,8 @@ class App extends Component {
         super();
         this.formatEntry = this.formatEntry.bind(this);
         this.setState({
-            entryText: 'Loading...'
+            entryText: 'Loading...',
+            copied: true
         });
     }
 
@@ -28,7 +29,13 @@ class App extends Component {
     }
 
     render(_, state) {
-        return (<div id="app-root"><p><span class="highlight">{state.entryText}</span></p></div>)
+        return (<div id="app-root">
+            <p><span class="highlight">{state.entryText}</span></p>
+            {state.copied && <div class="copied-confirmation">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
+                Copied to clipboard
+            </div>}
+        </div>)
     }
 }
 
